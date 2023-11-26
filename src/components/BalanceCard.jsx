e21552a124573aed28eeb261fcfc7424e331a93c
Adil Shaikh <shaikhadil881@gmail.com>
2023-11-26 16:06:02 +0100

    minor change

--- src/components/BalanceCard.jsx
+++ src/components/BalanceCard.jsx
@@ -1,4 +1,4 @@
-import { Container, Paper, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectBalance } from "../selectors";
@@ -6,27 +6,22 @@ import { selectBalance } from "../selectors";
 const BalanceCard = () => {
   const balance = useSelector(selectBalance);
   return (
-    <Container sx={{ display: "flex", justifyContent: "center" }}>
-      <Paper
-        elevation={4}
-        className="paper-backgroundImage"
-        sx={{
-          height: "250px",
-          borderRadius: "40px",
-          width: "460px",
-          display: "flex",
-          alignContent: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        <Typography
-          sx={{ pl: 2, zIndex: 1, bgcolor: "#72ce87c2" }}
-          variant="h4"
-        >
-          Balance: {balance}
-        </Typography>
-      </Paper>
-    </Container>
+    <Paper
+      elevation={4}
+      className="paper-backgroundImage"
+      sx={{
+        height: "250px",
+        borderRadius: "40px",
+        width: "460px",
+        display: "flex",
+        alignContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      <Typography sx={{ pl: 2, zIndex: 1, bgcolor: "#72ce87c2" }} variant="h4">
+        Balance: {balance}
+      </Typography>
+    </Paper>
   );
 };
 
